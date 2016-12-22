b53ccb0be695d1f6e30169dc3ef15a55e226c795
luiz henrique dandoline dos reis <luiz henrique dandoline dos reis>
2016-12-22 17:24:21 -0200

    Lambdas

--- src/br/com/alura/java8/OrdenaString.java
+++ src/br/com/alura/java8/OrdenaString.java
@@ -1,23 +1,19 @@
 package br.com.alura.java8;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaString {
 	public static void main(String[] args) {
-		List<String> list = Arrays.asList("Alura", "Caelum", "Casa do Codigo");
+		List<String> list = Arrays.asList("Caelum", "Casa do Codigo", "Alura");
 
-		Comparator<String> comparador = new ComparadorDeStringsPeloTamanho();
+		new ComparadorDeStringsPeloTamanho();
 
-		list.sort(comparador);
+		list.sort((s1, s2) -> s1.length() - s2.length());
 
 		System.out.println(list);
 
-		Consumer<String> consumidorDeStrings = new ConsumidorDeStrings();
-
-		list.forEach(consumidorDeStrings);
+		list.forEach(s -> System.out.println(s));
 
 	}
 
