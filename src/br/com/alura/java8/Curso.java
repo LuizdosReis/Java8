8a2f3e68799f7591eb7e657810df62d451b40d0a
Luiz Henrique <Luiz Henrique@DESKTOP-68A3ODO>
2016-12-29 22:12:08 -0200

    Streams

---
+++ src/br/com/alura/java8/Curso.java
@@ -0,0 +1,23 @@
+package br.com.alura.java8;
+
+public class Curso {
+	private String nome;
+	private int QtAlunos;
+
+	public Curso(String nome, int qtAlunos) {
+		super();
+		this.nome = nome;
+		QtAlunos = qtAlunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getQtAlunos() {
+		return QtAlunos;
+	}
+
+}
+
+
---
+++ src/br/com/alura/java8/Stream.java
@@ -0,0 +1,30 @@
+package br.com.alura.java8;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Stream {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Pyton", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		int alunos = cursos.stream().mapToInt(Curso::getQtAlunos).sum();
+
+		System.out.println(alunos);
+
+		cursos.sort(Comparator.comparingInt(Curso::getQtAlunos));
+
+		// cursos.forEach(c -> System.out.println(c.getNome()));
+
+		cursos.stream().map(Curso::getNome).forEach(s -> System.out.println(s));
+		System.out.println(cursos.stream().max(Comparator.comparingInt(Curso::getQtAlunos)).get().getNome());
+		// cursos.stream().filter(c -> c.getQtAlunos() >= 50).forEach(c ->
+		// System.out.println(c.getNome()));
+
+	}
+}
