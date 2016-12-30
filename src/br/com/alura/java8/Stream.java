4c56e9c826c6b18ab6e76dcc8d051e87cdee7bf2
LuizdosReis <luizdosreis@gmail.com>
2016-12-29 23:33:39 -0200

    Mais de Stream

--- src/br/com/alura/java8/Stream.java
+++ src/br/com/alura/java8/Stream.java
@@ -4,6 +4,9 @@ package br.com.alura.java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class Stream {
 	public static void main(String[] args) {
@@ -21,10 +24,22 @@ public class Stream {
 
 		// cursos.forEach(c -> System.out.println(c.getNome()));
 
-		cursos.stream().map(Curso::getNome).forEach(s -> System.out.println(s));
-		System.out.println(cursos.stream().max(Comparator.comparingInt(Curso::getQtAlunos)).get().getNome());
-		// cursos.stream().filter(c -> c.getQtAlunos() >= 50).forEach(c ->
-		// System.out.println(c.getNome()));
+		// cursos.stream().map(Curso::getNome).forEach(s ->
+		// System.out.println(s));
+		// cursos.stream().max(Comparator.comparingInt(Curso::getQtAlunos))
+		// .ifPresent(c -> System.out.println(c.getNome()));
+
+		cursos.stream().filter(c -> c.getQtAlunos() >= 50).collect(Collectors.toList());
+
+		Map<String, Integer> collect = cursos.stream()
+				.filter(c -> c.getQtAlunos() >= 50)
+				.collect(Collectors.toMap(Curso::getNome, Curso::getQtAlunos));
+
+		collect.keySet().forEach(System.out::println);
+
+		OptionalDouble average = cursos.stream().mapToInt(Curso::getQtAlunos).average();
+		
+		System.out.println(average.orElse(-100));
 
 	}
 }
